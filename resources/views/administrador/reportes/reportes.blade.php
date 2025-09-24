c3529c9aa50c762bbfbb61f301fc8790896adbee
Vyda Calderon <crvo220051@upemor.edu.mx>
2025-09-24 13:42:07 -0600

    Diseño de los reportes mejorado.

--- resources/views/administrador/reportes/reportes.blade.php
+++ resources/views/administrador/reportes/reportes.blade.php
@@ -2,14 +2,81 @@
 @section('title', 'Reportes')
 
 @section('content')
-  <h1>Reportes</h1>
-  <section class="reports-grid">
-    <a href="{{ route('reportes.inscritos.index') }}" class="report-card">Reporte de alumnos inscritos por diplomado</a>
-    <a href="{{ route('admin.reportes.alumnosEdad.index') }}" class="report-card">Reporte de alumnos inscritos por edad</a>
-    <a href="{{ route('reportes.alumnos.concluidos') }}" class="report-card">Reporte de alumnos con diplomado concluido</a>
-    <a href="{{ route('reportes.pagos') }}" class="report-card">Reporte de pagos (semanales y mensuales)</a>
-    <a href="{{ route('reportes.adeudos') }}" class="report-card">Reporte de adeudos</a>
-    <a href="{{ route('reportes.reprobados.index') }}" class="report-card">Reporte de alumnos reprobados</a>
-    <a href="{{ route('reportes.aspirantes.index') }}" class="report-card">Reporte de aspirantes interesados en diplomado</a>
-  </section>
+  <div class="reports-container">
+    <h1>Panel de Reportes</h1>
+    
+    <ul class="reports-list">
+      
+      <li class="report-item">
+        <a href="{{ route('reportes.inscritos.index') }}" class="report-link">
+          <div class="report-icon">✔</div>
+          <div class="report-link-content">
+            <h3 class="report-title">Alumnos Inscritos por Diplomado</h3>
+            <p class="report-description">Reporte de alumnos inscritos por diplomado</p>
+          </div>
+        </a>
+      </li>
+      
+      <li class="report-item">
+        <a href="{{ route('admin.reportes.alumnosEdad.index') }}" class="report-link">
+          <div class="report-icon">✔</div>
+          <div class="report-link-content">
+            <h3 class="report-title">Alumnos por Edad</h3>
+            <p class="report-description">Reporte de alumnos inscritos agrupados por edad</p>
+          </div>
+        </a>
+      </li>
+      
+      <li class="report-item">
+        <a href="{{ route('reportes.alumnos.concluidos') }}" class="report-link">
+          <div class="report-icon">✔</div>
+          <div class="report-link-content">
+            <h3 class="report-title">Diplomados Concluidos</h3>
+            <p class="report-description">Reporte de alumnos con diplomado concluido</p>
+          </div>
+        </a>
+      </li>
+      
+      <li class="report-item">
+        <a href="{{ route('reportes.pagos') }}" class="report-link">
+          <div class="report-icon">✔</div>
+          <div class="report-link-content">
+            <h3 class="report-title">Reporte de Pagos</h3>
+            <p class="report-description">Semanales y mensuales</p>
+          </div>
+        </a>
+      </li>
+      
+      <li class="report-item">
+        <a href="{{ route('reportes.adeudos') }}" class="report-link">
+          <div class="report-icon">✔</div>
+          <div class="report-link-content">
+            <h3 class="report-title">Reporte de Adeudos</h3>
+            <p class="report-description">Alumnos con pagos pendientes</p>
+          </div>
+        </a>
+      </li>
+      
+      <li class="report-item">
+        <a href="{{ route('reportes.reprobados.index') }}" class="report-link">
+          <div class="report-icon">✔</div>
+          <div class="report-link-content">
+            <h3 class="report-title">Alumnos Reprobados</h3>
+            <p class="report-description">Análisis por módulo</p>
+          </div>
+        </a>
+      </li>
+      
+      <li class="report-item">
+        <a href="{{ route('reportes.aspirantes.index') }}" class="report-link">
+          <div class="report-icon">✔</div>
+          <div class="report-link-content">
+            <h3 class="report-title">Aspirantes Interesados</h3>
+            <p class="report-description">Por diplomado</p>
+          </div>
+        </a>
+      </li>
+
+    </ul>
+  </div>
 @endsection
